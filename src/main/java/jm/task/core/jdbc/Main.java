d396cb250cabe9f4ffab3e2a319e52bb2a657c50
AndySpinko <spdale016@gmail.com>
2021-07-30 20:37:10 +0700

    is done

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,22 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+    public static void main(String[] args) throws SQLException {
+
+        UserService userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+        userService.saveUser("Matt", "Bellamy", (byte) 41);
+        userService.saveUser("Tom", "Chaplin", (byte) 35);
+        userService.saveUser("Jack", "White", (byte) 21);
+        userService.saveUser("Brian", "Molko", (byte) 53);
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,27 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+
+    private final String URL = "jdbc:mysql://localhost:3306/mytest";
+    private final String USERNAME = "root";
+    private final String PASS = "exlibris";
+    private final String DRIVER = "com.mysql.cj.jdbc.Driver";
+
+    public Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName(DRIVER);
+            connection = DriverManager.getConnection(URL, USERNAME, PASS);
+            System.out.println("Connected");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            System.out.println("Connection failed");
+        }
+        return connection;
+    }
 }
+
